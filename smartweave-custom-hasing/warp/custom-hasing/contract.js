bee540b75b7c45a6626a919d021e30056bb04fe8
Harris Levine <acyclicgraphs@gmail.com>
2022-10-11 14:46:53 -0400

    Begin custom hashing contract

---
+++ smartweave-custom-hasing/warp/custom-hasing/contract.js
@@ -0,0 +1,14 @@
+/* warp/contract.js */
+export function handle(state, action) {
+    /* address of the caller is available in action.caller */
+    if (action.input.function === 'initialize') {
+      state.hasher = action.caller
+    }
+    if (action.input.function === 'accept7CharacterString' && action.caller === state.hasher) {
+        // @TODO
+    }
+    if (action.input.function === 'returnCIDCorrespondingContent' && action.caller === state.hasher) {
+        // @TODO
+    }
+    return { state }
+}
